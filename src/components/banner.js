e01a6b7ac239abc8bed54d86927246f28bb0746e
1wes <okemwawes@gmail.com>
2023-08-24 12:21:12 +0300

    implement tagline and hero-sections

--- src/components/banner.js
+++ src/components/banner.js
@@ -1,13 +1,28 @@
-import React from 'react';
+import React from "react";
 import './banner.css';
+import capsule from '../assets/capsule.jpg'
 
 const Banner=()=>{
 
     return(
         <React.Fragment>
-            <nav>
-                AM the banner
-            </nav>
+            <main className="banner">
+                <section className="hero-tagline">
+                    <div>
+                        <h1 className="tagline">
+                            We enable Space Travel and Exploration
+                        </h1>
+                        <h2 className="tagline-support">
+                            Learn all about our capsules, which are enabling
+                            the process of humans become multi-planetary species
+                            by sending them and cargo into space.
+                        </h2>
+                    </div>
+                </section>
+                <section className="hero-image">
+                    <img src={capsule} alt="capsule-in-orbit" />
+                </section>
+            </main>
         </React.Fragment>
     )
 }
