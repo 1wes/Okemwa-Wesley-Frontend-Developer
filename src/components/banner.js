d61bd7bb045bae3413089a3ef77e7b1a494f1863
1wes <okemwawes@gmail.com>
2023-08-24 11:09:59 +0300

    initial folder structure

---
+++ src/components/banner.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './banner.css';
+
+const Banner=()=>{
+
+    return(
+        <React.Fragment>
+            <nav>
+                AM the banner
+            </nav>
+        </React.Fragment>
+    )
+}
+export default Banner;
