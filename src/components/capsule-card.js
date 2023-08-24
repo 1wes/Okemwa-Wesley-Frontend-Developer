853ccbe330c29f9530900d69c72f32faa51ca542
1wes <okemwawes@gmail.com>
2023-08-24 18:28:55 +0300

    export popup

--- src/components/capsule-card.js
+++ src/components/capsule-card.js
@@ -1,14 +1,28 @@
 import React from "react";
 import './capsule-card.css';
 
-const Card=({name, missions, status, details, onClick, serial, type, id, laaunchDate, landings, reuse, missionNames})=>{
+const MoreData=({serial, type, id, laaunchDate, landings, reuse, missionNames, status})=>{
 
-    const closePopUp=()=>{
+    return(
+        <React.Fragment>
+                <div className="item-pop-up" id="popup">
+                    <ul className="additional-data">
+                        <li>Capsule serial : <span>{serial}</span></li>
+                        <li>Capsule type : <span>{type}</span></li>
+                        <li>Capsule ID : <span>{id}</span></li>
+                        <li>Status : <span>{status}</span></li>
+                        <li>Original launch : <span>{laaunchDate}</span></li>
+                        <li>Missions : {missionNames}</li>
+                        <li>Landings : <span>{landings}</span></li>
+                        <li>Reuse count : <span>{reuse}</span></li>
+                    </ul>
+                    <button className="close">Close</button>
+                </div>
+        </React.Fragment>
+    )
+}
 
-        const popup=document.getElementById("popup");
-
-        popup.classList.add("close-popup")
-    }
+const Card=({children,name, missions, status, details, onClick})=>{
 
     return(
         <>
@@ -29,22 +43,12 @@ const Card=({name, missions, status, details, onClick, serial, type, id, laaunch
                     {details}
                 </p>
                 </div>
-                <div className="item-pop-up" id="popup">
-                    <ul className="additional-data">
-                        <li>Capsule serial : <span>{serial}</span></li>
-                        <li>Capsule type : <span>{type}</span></li>
-                        <li>Capsule ID : <span>{id}</span></li>
-                        <li>Status : <span>{status}</span></li>
-                        <li>Original launch : <span>{laaunchDate}</span></li>
-                        <li>Missions : {missionNames}</li>
-                        <li>Landings : <span>{landings}</span></li>
-                        <li>Reuse count : <span>{reuse}</span></li>
-                    </ul>
-
-                    <button className="close" onClick={closePopUp}>Close</button>
-                </div>
+                {children}
             </div>
         </>
     )
+}
+export{
+    MoreData
 }
 export default Card;
