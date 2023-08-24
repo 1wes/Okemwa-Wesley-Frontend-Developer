7742b5d9ec7d0861796ac8951a2749ba52ecc896
1wes <okemwawes@gmail.com>
2023-08-24 13:13:31 +0300

    hierachically add components for page layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import Navbar from './components/navbar';
+import Banner from './components/banner';
+import Capsules from './components/data-grid';
 
 function App() {
   return (
     <div className="App">
-      Am app
+      <Navbar/>
+      <Banner/>
+      <Capsules/>
     </div>
   );
 }
